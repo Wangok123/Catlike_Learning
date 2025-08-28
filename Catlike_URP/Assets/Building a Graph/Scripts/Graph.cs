a8e291c0ba5ff3dc8f61196ba54fa4ec853f91e6
Wangok123 <henryok123>
2025-08-28 10:41:20 +0800

    feat：准备Creating More Cubes

---
+++ Catlike_URP/Assets/Building a Graph/Scripts/Graph.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Building_a_Graph.Scripts
+{
+    public class Graph : MonoBehaviour
+    {
+        [SerializeField]
+        Transform pointPrefab;
+        
+        void Awake () {
+            Vector3 position = Vector3.zero;
+            var scale = Vector3.one / 5f;
+            for (int i = 0; i < 10; i++) {
+                Transform point = Instantiate(pointPrefab);
+                position.x = (i + 0.5f) / 5f - 1f;
+                position.y = position.x * position.x;
+                point.localPosition = position; 
+                point.localScale = scale;
+            }
+        }
+    }
+}
