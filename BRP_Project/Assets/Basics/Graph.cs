5b7a6fba2f6d3f4362692779f15cc0e174d1be9a
wanghan <>
2025-09-02 20:19:56 +0800

    feat：完成随机表现，准备加入插值

--- BRP_Project/Assets/Basics/Graph.cs
+++ BRP_Project/Assets/Basics/Graph.cs
@@ -7,20 +7,65 @@ namespace Basics
         [SerializeField] Transform pointPrefab;
         [SerializeField, Range(10, 100)] int resolution = 10;
 
+        [SerializeField] FunctionLibrary.FunctionName function;
+        
+        public enum TransitionMode { Cycle, Random }
+
+        [SerializeField]
+        TransitionMode transitionMode;
+        
+        [SerializeField, Min(0f)]
+        float functionDuration = 1f;
+
+        Transform[] points;
+
+        float duration;
+        
         void Awake()
         {
             float step = 2f / resolution;
-            var position = Vector3.zero;
             var scale = Vector3.one * step;
-            for (int i = 0; i < resolution; i++)
-            {
-                Transform point = Instantiate(pointPrefab);
-                position.x = (i + 0.5f) * step - 1f;
-                position.y = position.x * position.x * position.x;
-                point.localPosition = position;
+
+            points = new Transform[resolution * resolution];
+            for (int i = 0; i < points.Length; i++) {
+                Transform point = points[i] = Instantiate(pointPrefab);
                 point.localScale = scale;
                 point.SetParent(transform, false);
             }
+        }
+
+        void Update()
+        {
+            duration += Time.deltaTime;
+            if (duration >= functionDuration) {
+                duration -= functionDuration;
+                PickNextFunction();
+            }
+            
+            UpdateFunction();
+        }
+        
+        void UpdateFunction () {
+            FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
+
+            float time = Time.time;
+            float step = 2f / resolution;
+            float v = 0.5f * step - 1f;
+            for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
+                if (x == resolution) {
+                    x = 0;
+                    z += 1;
+                    v = (z + 0.5f) * step - 1f;
+                }
+                float u = (x + 0.5f) * step - 1f;
+                points[i].localPosition = f(u, v, time);
+            }
+        }
+        
+        void PickNextFunction () {
+            function = transitionMode == TransitionMode.Cycle ?
+                FunctionLibrary.GetNextFunctionName(function) :
+                FunctionLibrary.GetRandomFunctionNameOtherThan(function);
         }
     }
 }
