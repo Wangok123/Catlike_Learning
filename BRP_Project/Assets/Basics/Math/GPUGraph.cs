6fa50023db948c7f1efc83c695de1448d044d7b0
Wangok123 <henryok123>
2025-09-04 00:37:37 +0800

    compute shader感觉有点晦涩 暂时跳过

--- BRP_Project/Assets/Basics/Math/GPUGraph.cs
+++ BRP_Project/Assets/Basics/Math/GPUGraph.cs
@@ -8,6 +8,20 @@ namespace Basics
 
         [SerializeField] FunctionLibrary.FunctionName function;
 
+        [SerializeField] ComputeShader computeShader;
+
+        [SerializeField]
+        Material material;
+
+        [SerializeField]
+        Mesh mesh;
+        
+        static readonly int
+            positionsId = Shader.PropertyToID("_Positions"),
+            resolutionId = Shader.PropertyToID("_Resolution"),
+            stepId = Shader.PropertyToID("_Step"),
+            timeId = Shader.PropertyToID("_Time");
+        
         public enum TransitionMode
         {
             Cycle,
@@ -18,6 +32,8 @@ namespace Basics
 
         [SerializeField, Min(0f)] float functionDuration = 1f, transitionDuration = 1f;
 
+
+        
         float duration;
 
         bool transitioning;
@@ -25,16 +41,18 @@ namespace Basics
         FunctionLibrary.FunctionName transitionFunction;
 
         ComputeBuffer positionsBuffer;
-        
-        void OnEnable () {
-            positionsBuffer = new ComputeBuffer(resolution * resolution, 3 * 4);  // float3
+
+        void OnEnable()
+        {
+            positionsBuffer = new ComputeBuffer(resolution * resolution, 3 * 4); // float3
         }
-        
-        void OnDisable () {
+
+        void OnDisable()
+        {
             positionsBuffer.Release();
             positionsBuffer = null;
         }
-        
+
         void Update()
         {
             duration += Time.deltaTime;
@@ -53,6 +71,22 @@ namespace Basics
                 transitionFunction = function;
                 PickNextFunction();
             }
+            
+            UpdateFunctionOnGPU();
+        }
+        
+        void UpdateFunctionOnGPU () {
+            float step = 2f / resolution;
+            computeShader.SetInt(resolutionId, resolution);
+            computeShader.SetFloat(stepId, step);
+            computeShader.SetFloat(timeId, Time.time);
+            		
+            computeShader.SetBuffer(0, positionsId, positionsBuffer);
+            int groups = Mathf.CeilToInt(resolution / 8f);
+            computeShader.Dispatch(0, groups, groups, 1);
+            
+            var bounds = new Bounds(Vector3.zero, Vector3.one * (2f + 2f / resolution));
+            Graphics.DrawMeshInstancedProcedural(mesh, 0, material, bounds, positionsBuffer.count);
         }
 
 
