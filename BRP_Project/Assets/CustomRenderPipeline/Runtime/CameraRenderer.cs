d6a2533bbb6c21fdc213142f2579465d1dd1412e
Wangok123 <henryok123>
2025-09-25 03:11:19 +0800

    feat：完成基础渲染

--- BRP_Project/Assets/CustomRenderPipeline/Runtime/CameraRenderer.cs
+++ BRP_Project/Assets/CustomRenderPipeline/Runtime/CameraRenderer.cs
@@ -8,32 +8,39 @@ namespace CustomRenderPipeline.Runtime
         static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
 
         private const string bufferName = "Render Camera";
-        
+
         ScriptableRenderContext context;
         CullingResults cullingResults;
         Camera camera;
 
-        CommandBuffer buffer = new CommandBuffer {
+        CommandBuffer buffer = new CommandBuffer
+        {
             name = bufferName
         };
 
-
-        public void Render (ScriptableRenderContext context, Camera camera) {
+        public void Render(ScriptableRenderContext context, Camera camera)
+        {
             this.context = context;
             this.camera = camera;
-            
-            if (!Cull()) {
+
+            PrepareBuffer();
+            PrepareForSceneWindow();
+            if (!Cull())
+            {
                 return;
             }
-            
+
             Setup();
             DrawVisibleGeometry();
             DrawUnsupportedShaders();
+            DrawGizmos();
             Submit();
         }
-        
-        void DrawVisibleGeometry () {
-            var sortingSettings = new SortingSettings(camera){
+
+        void DrawVisibleGeometry()
+        {
+            var sortingSettings = new SortingSettings(camera)
+            {
                 criteria = SortingCriteria.CommonOpaque
             };
             var drawingSettings = new DrawingSettings(unlitShaderTagId, sortingSettings);
@@ -42,9 +49,9 @@ namespace CustomRenderPipeline.Runtime
             context.DrawRenderers(
                 cullingResults, ref drawingSettings, ref filteringSettings
             );
-            
+
             context.DrawSkybox(camera);
-            
+
             sortingSettings.criteria = SortingCriteria.CommonTransparent;
             drawingSettings.sortingSettings = sortingSettings;
             filteringSettings.renderQueueRange = RenderQueueRange.transparent;
@@ -53,33 +60,39 @@ namespace CustomRenderPipeline.Runtime
                 cullingResults, ref drawingSettings, ref filteringSettings
             );
         }
-        
-        void Setup () {
+
+        void Setup()
+        {
             context.SetupCameraProperties(camera);
-            buffer.ClearRenderTarget(true, true, Color.clear);
-            buffer.BeginSample(bufferName);
+            CameraClearFlags flags = camera.clearFlags;
+            buffer.ClearRenderTarget(flags <= CameraClearFlags.Depth, flags <= CameraClearFlags.Color,
+                flags == CameraClearFlags.Color ? camera.backgroundColor.linear : Color.clear);
+            buffer.BeginSample(SampleName);
             ExecuteBuffer();
         }
-        
-        void Submit () {
-            buffer.EndSample(bufferName);
+
+        void Submit()
+        {
+            buffer.EndSample(SampleName);
             ExecuteBuffer();
             context.Submit();
         }
-        
-        void ExecuteBuffer () {
+
+        void ExecuteBuffer()
+        {
             context.ExecuteCommandBuffer(buffer);
             buffer.Clear();
         }
-        
-        bool Cull ()
+
+        bool Cull()
         {
-            if (camera.TryGetCullingParameters(out ScriptableCullingParameters p)) {
+            if (camera.TryGetCullingParameters(out ScriptableCullingParameters p))
+            {
                 cullingResults = context.Cull(ref p);
                 return true;
             }
+
             return false;
         }
-        
     }
 }
